2d34a4fd95715880d31139148ad1d58dc837c669
NFinks <nicolefinks@utexas.edu>
2018-11-14 22:49:51 -0600

    Update SplashPage.java

--- SplashPage.java
+++ SplashPage.java
@@ -4,14 +4,22 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class SplashPage extends Instance implements Serializable {
-	private SplashPage();
+	
+	private String[] feed;
+	private String videoURL;
+	
+	private SplashPage(){};
   
 	private static class SplashPageHolder {
-    private static final SplashPage splash = new SplashPage();
+    		private static final SplashPage splash = new SplashPage();
+		String[] dummy;
+		String dummyURL;
+		splash.feed = dummy;
+		splash.videoURL = dummyURL; 
   
   } 
 
 	public static SplashPage getInstanceOfSplashPage(){
-    return SplashPageHolder.splash;
+   		return SplashPageHolder.splash;
     }
 }
